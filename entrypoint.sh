9ac8f7993ff278b10debcec873d4ab4b21de87f1
hvalev <hvalev@yahoo.de>
2024-07-15 18:29:40 +0200

    updated entrypoint.sh to take parameters from file or environmental variables. Fixed bug for not releasing fs handle on container stop

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,16 +1,46 @@
 #!/bin/sh
 
-function cleanup {
+# Function to clean up on script exit
+cleanup() {
+    echo "Unmounting mergerfs..."
     umount /merged
+    echo "Unmounted mergerfs."
 }
 
-mergerfs -o allow_other,use_ino,cache.files=partial,dropcacheonclose=true,moveonenospc=true,category.create=mfs /disks/*: /merged
+# Set debugging options
+# set -e  # Exit immediately if a command exits with a non-zero status
+# set -x  # Print commands and their arguments as they are executed
 
+# Define the default parameters file
+PARAMETERS_FILE="/config/parameters.conf"
 
+# Check if MERGERFS_PARAMS is set, override PARAMETERS_FILE if it is
+if [ -n "$MERGERFS_PARAMS" ]; then
+    echo "Using parameters from MERGERFS_PARAMS environment variable."
+    echo "$MERGERFS_PARAMS" > "$PARAMETERS_FILE"
+fi
+
+# Merge parameters from the file into a single line without comments or empty lines
+PARAMS=$(grep -v '^#' "$PARAMETERS_FILE" | sed '/^\s*$/d' | sed 's/^[ \t]*//;s/[ \t]*$//' | paste -sd ',' -)
+
+# Prepare the mergerfs command
+COMMAND="mergerfs -o $PARAMS /disks/*: /merged"
+
+# Print the command being executed
+echo "Executing: $COMMAND"
+
+# Mount using mergerfs
+$COMMAND
+
+# Trap SIGTERM signal of docker daemon
+trap cleanup SIGTERM
+
+# Trap exit signals to ensure cleanup
 trap cleanup EXIT INT
 
+# Wait for mergerfs process to finish
 while [[ $(ps -eo 'comm' | grep mergerfs -c) -gt 0 ]]; do
     sleep 1
 done
 
-echo mergerfs terminated
+echo "mergerfs terminated"
